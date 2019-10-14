b10dbd1cfeed2768a39aa91a7e90e7b107efccd6
rungene <rungenesamuel@gmail.com>
2019-10-14 18:14:06 +0300

    Navigation screen to screen

---
+++ app/src/main/java/com/rungenes/rent_a_boat/HomeFragment.kt
@@ -0,0 +1,33 @@
+package com.rungenes.rent_a_boat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class homeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val navHostFragment =childFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+
+        view.findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+            .setupWithNavController(navController = navHostFragment.navController )
+
+
+        return view
+    }
+
+
+}
